a2ff73b835aebdb443c8b9e75e241c55e77ec86a
raszion23 <rastazion23@gmail.com>
2020-04-09 17:00:21 +0200

    First Commit

---
+++ part06-Part06_13.Exercises/src/main/java/Exercise.java
@@ -0,0 +1,30 @@
+
+/**
+ * File: Exercise.java
+ * Author: Anthony Francis
+ * Date: Apr 9, 2020
+ * Purpose:
+ */
+public class Exercise {
+
+    private String name;
+    private boolean completed;
+
+    public Exercise(String name) {
+        this.name = name;
+        this.completed = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+}
